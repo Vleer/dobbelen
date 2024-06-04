6a2175be192f5413086d9e9beda02c58974a914d
Vleer <Vleer@users.noreply.github.com>
2024-06-04 12:09:42 +0200

    initial commit

---
+++ dobbelen-backend/dobbelen-backend/src/main/java/com/example/dobbelen_backend/DobbelenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dobbelen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobbelenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobbelenBackendApplication.class, args);
+	}
+
+}
