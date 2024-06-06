43c89613f635518cc796e00e26030705cb0aadaf
vleer <Vleertouwer@gmail.com>
2024-06-07 00:46:31 +0200

    dice added

--- dobbelen-frontend/src/App.tsx
+++ dobbelen-frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
 import DataFetcher from "./components/DataFetcher";
 import DataPoster from "./components/DataPoster";
+import DiceHand from "./components/DiceHand";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-700 p-4 text-white">
       <DataFetcher />
       <DataPoster />
+      <DiceHand diceValues={[1, 2, 3, 4, 5]} />
+      {/* const [diceValues, setDiceValues] = useState<number[]>([1, 2, 3, 4, 5]); */}
     </div>
   );
 }
