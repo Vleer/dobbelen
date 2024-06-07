9e8808ab31a5fcc7a04734e057e1234590138365
vleer <Vleertouwer@gmail.com>
2024-06-07 09:50:52 +0200

    Dice now work visually

--- dobbelen-frontend/src/App.tsx
+++ dobbelen-frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import DataFetcher from "./components/DataFetcher";
 import DataPoster from "./components/DataPoster";
 import DiceHand from "./components/DiceHand";
+import React, { useState } from "react";
 
 function App() {
+  const [diceValues, setDiceValues] = useState<number[]>([1, 2, 3, 4, 5, 6, 5]);
   return (
     <div className="App bg-blue-700 p-4 text-white">
       <DataFetcher />
       <DataPoster />
-      <DiceHand diceValues={[1, 2, 3, 4, 5]} />
-      {/* const [diceValues, setDiceValues] = useState<number[]>([1, 2, 3, 4, 5]); */}
+      <h1>Dice Game</h1>
+      <DiceHand diceValues={diceValues} />
     </div>
   );
 }
--- dobbelen-frontend/src/components/Dice.tsx
+++ dobbelen-frontend/src/components/Dice.tsx
@@ -1,5 +1,4 @@
-// Dice.tsx
-import React from 'react';
+import React from "react";
 
 interface DiceProps {
   value: number;
@@ -7,17 +6,138 @@ interface DiceProps {
 
 const Dice: React.FC<DiceProps> = ({ value }) => {
   const renderDots = (num: number) => {
-    let dots = [];
-    for (let i = 0; i < num; i++) {
-      dots.push(
-        <div key={i} className="w-2 h-2 bg-black rounded-full absolute"></div>
-      );
+    switch (num) {
+      case 1:
+        return (
+          <div
+            className="w-2 h-2 bg-black rounded-full absolute"
+            style={{
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          ></div>
+        );
+      case 2:
+        return (
+          <>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "25%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ bottom: "25%", right: "25%" }}
+            ></div>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "25%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ bottom: "25%", right: "25%" }}
+            ></div>
+          </>
+        );
+      case 4:
+        return (
+          <>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "25%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "25%", right: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ bottom: "25%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ bottom: "25%", right: "25%" }}
+            ></div>
+          </>
+        );
+      case 5:
+        return (
+          <>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "25%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "25%", right: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ bottom: "25%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ bottom: "25%", right: "25%" }}
+            ></div>
+          </>
+        );
+      case 6:
+        return (
+          <>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "20%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "20%", right: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "40%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ top: "40%", right: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ bottom: "25%", left: "25%" }}
+            ></div>
+            <div
+              className="w-2 h-2 bg-black rounded-full absolute"
+              style={{ bottom: "25%", right: "25%" }}
+            ></div>
+          </>
+        );
+      default:
+        return null;
     }
-    return dots;
   };
 
   return (
-    <div className="relative w-12 h-12 bg-white border border-black flex flex-wrap items-center justify-center m-1">
+    <div className="relative w-12 h-12 bg-white border border-black rounded-lg flex items-center justify-center m-1">
       {renderDots(value)}
     </div>
   );
